f2ce9f6f43d6a75f6f036980703f96a5aa82b54a
Pawel Niewiadomski <11110000b@gmail.com>
2013-03-24 23:01:41 +0100

    Added copyright notice for two methods copied from an external library

--- Buffered Example/AppDelegate.m
+++ Buffered Example/AppDelegate.m
@@ -257,6 +257,20 @@ static NSString *DRAG_AND_DROP_TYPE = @"Update Data";
     return NO;
 }
 
+// RSRTVArrayController.m
+//
+// RSRTV stands for Red Sweater Reordering Table View Controller.
+//
+// Based on code from Apple's DragNDropOutlineView example, which granted
+// unlimited modification and redistribution rights, provided Apple not be held legally liable.
+//
+// Differences between this file and the original are © 2006 Red Sweater Software.
+//
+// You are granted a non-exclusive, unlimited license to use, reproduce, modify and
+// redistribute this source code in any form provided that you agree to NOT hold liable
+// Red Sweater Software or Daniel Jalkut for any damages caused by such use.
+//
+
 - (NSUInteger)rowsAboveRow:(NSUInteger)row inIndexSet:(NSIndexSet *)indexSet
 {
     NSUInteger currentIndex = [indexSet firstIndex];
@@ -268,6 +282,20 @@ static NSString *DRAG_AND_DROP_TYPE = @"Update Data";
     }
     return i;
 }
+
+// RSRTVArrayController.m
+//
+// RSRTV stands for Red Sweater Reordering Table View Controller.
+//
+// Based on code from Apple's DragNDropOutlineView example, which granted
+// unlimited modification and redistribution rights, provided Apple not be held legally liable.
+//
+// Differences between this file and the original are © 2006 Red Sweater Software.
+//
+// You are granted a non-exclusive, unlimited license to use, reproduce, modify and
+// redistribute this source code in any form provided that you agree to NOT hold liable
+// Red Sweater Software or Daniel Jalkut for any damages caused by such use.
+//
 
 -(void) moveObjectsInArrangedObjectsFromIndexes:(NSIndexSet*)indexSet toIndex:(NSUInteger)insertIndex
 {
